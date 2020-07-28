c8350c114aeeb5ce75a202f559ea0f8250ccdb91
JoongSeokD <ljseokd@gmail.com>
2020-07-28 19:56:39 +0900

    로그인 페이지 permitAll

--- src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
+++ src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().mvcMatchers("/").permitAll()
+                .authorizeRequests().mvcMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated();
 
         http

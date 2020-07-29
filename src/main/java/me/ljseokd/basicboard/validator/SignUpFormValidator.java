80a613a8438a24e36f142964f3a8c45059469879
JoongSeokD <ljseokd@gmail.com>
2020-07-29 09:55:44 +0900

    회원 가입 데이터 폼 유효성 검사

---
+++ src/main/java/me/ljseokd/basicboard/validator/SignUpFormValidator.java
@@ -0,0 +1,30 @@
+package me.ljseokd.basicboard.validator;
+
+import lombok.RequiredArgsConstructor;
+import me.ljseokd.basicboard.domain.Account;
+import me.ljseokd.basicboard.form.SignUpForm;
+import me.ljseokd.basicboard.repository.AccountRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm) target;
+        Account account = accountRepository.findByName(signUpForm.getUsername());
+        if (account != null){
+            errors.rejectValue("username", "invalid.username", new Object[]{signUpForm.getUsername()}, "이미 존재하는 이름입니다.");
+        }
+    }
+}

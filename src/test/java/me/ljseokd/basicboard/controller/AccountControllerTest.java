b10229bf73d52006415cc6363f0a1cc6e07ff52b
JoongSeokD <ljseokd@gmail.com>
2020-07-29 09:00:19 +0900

    회원 가입 페이지 테스트 케이스

--- src/test/java/me/ljseokd/basicboard/controller/AccountControllerTest.java
+++ src/test/java/me/ljseokd/basicboard/controller/AccountControllerTest.java
@@ -1,12 +1,17 @@
 package me.ljseokd.basicboard.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -18,12 +23,23 @@ class AccountControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @DisplayName("로그인 페이지")
     @Test
     void login() throws Exception {
         mockMvc
                 .perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login"))
-        ;
+                .andExpect(unauthenticated());
     }
+
+    @DisplayName("회원 가입 페이지")
+    @Test
+    void sign_up_form() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sign-up"))
+                .andExpect(unauthenticated());
+    }
+
 }

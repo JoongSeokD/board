1f9777ff4ebe62de2a52e2f2d9596b107d62d846
JoongSeokD <ljseokd@gmail.com>
2020-07-29 09:54:18 +0900

    회원 가입

--- src/main/java/me/ljseokd/basicboard/controller/AccountController.java
+++ src/main/java/me/ljseokd/basicboard/controller/AccountController.java
@@ -1,10 +1,31 @@
 package me.ljseokd.basicboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.ljseokd.basicboard.form.SignUpForm;
+import me.ljseokd.basicboard.service.AccountService;
+import me.ljseokd.basicboard.validator.SignUpFormValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/login")
     public String loginForm(){
@@ -12,7 +33,18 @@ public class AccountController {
     }
 
     @GetMapping("/sign-up")
-    public String signUpForm(){
+    public String signUpForm(Model model){
+        model.addAttribute(new SignUpForm());
         return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){
+
+        if (errors.hasErrors()){
+            return "sign-up";
+        }
+        accountService.join(signUpForm);
+        return "redirect:/";
     }
 }

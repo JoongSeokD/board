dbdf2ab4f7ea1f9893268c327a37876b24693bd3
JoongSeokD <ljseokd@gmail.com>
2020-07-29 09:54:32 +0900

    회원 가입 테스트 케이스

--- src/test/java/me/ljseokd/basicboard/controller/AccountControllerTest.java
+++ src/test/java/me/ljseokd/basicboard/controller/AccountControllerTest.java
@@ -1,27 +1,35 @@
 package me.ljseokd.basicboard.controller;
 
+import me.ljseokd.basicboard.domain.Account;
+import me.ljseokd.basicboard.repository.AccountRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountRepository accountRepository;
 
     @DisplayName("로그인 페이지")
     @Test
@@ -38,6 +46,51 @@ class AccountControllerTest {
     void sign_up_form() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("sign-up"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 성공")
+    @Test
+    void sign_up_success() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andDo(print())
+                .andExpect(model().hasNoErrors())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated())
+        ;
+
+        Account ljseokd = accountRepository.findByName("ljseokd");
+        Assertions.assertNotEquals("12345678",ljseokd.getPassword());
+    }
+    
+    @DisplayName("회원가입 실패 (중복된 이름)")
+    @Test
+    void sign_up_duplicate_fail() throws Exception {
+        accountRepository.save(new Account("ljseokd", "12345678"));
+
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(view().name("sign-up"))
+                .andExpect(unauthenticated());
+    }
+    @DisplayName("회원가입 실패 (비밀번호 길이 부족)")
+    @Test
+    void sign_up_password_length_fail() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "1234567")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
                 .andExpect(view().name("sign-up"))
                 .andExpect(unauthenticated());
     }

acbdcf75af33d4cd557d4885e13f3d3c515641f5
JoongSeokD <ljseokd@gmail.com>
2020-07-29 09:00:11 +0900

    회원 가입 페이지 인증 제외

--- src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
+++ src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().mvcMatchers("/", "/login").permitAll()
+                .authorizeRequests().mvcMatchers("/", "/login", "sign-up").permitAll()
                 .anyRequest().authenticated();
 
         http

58af37489de406591461260ebe17459f2c93f862
JoongSeokD <ljseokd@gmail.com>
2020-07-30 17:36:52 +0900

    게시글 보기 테스트 케이스 추가

--- src/test/java/me/ljseokd/basicboard/controller/NoticeControllerTest.java
+++ src/test/java/me/ljseokd/basicboard/controller/NoticeControllerTest.java
@@ -1,6 +1,14 @@
 package me.ljseokd.basicboard.controller;
 
 import me.ljseokd.basicboard.WithAccount;
+import me.ljseokd.basicboard.domain.Account;
+import me.ljseokd.basicboard.domain.Notice;
+import me.ljseokd.basicboard.form.NoticeForm;
+import me.ljseokd.basicboard.form.SignUpForm;
+import me.ljseokd.basicboard.repository.AccountRepository;
+import me.ljseokd.basicboard.repository.NoticeRepository;
+import me.ljseokd.basicboard.service.AccountService;
+import me.ljseokd.basicboard.service.NoticeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,6 +34,18 @@ class NoticeControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    NoticeService noticeService;
+
+    @Autowired
+    NoticeRepository noticeRepository;
 
     @DisplayName("게시글 등록 페이지")
     @Test
@@ -63,6 +85,40 @@ class NoticeControllerTest {
                 .andExpect(model().attributeExists("noticeForm"))
                 .andExpect(view().name("/notice/create-notice-form"))
                 .andExpect(authenticated());
+    }
+
+    @DisplayName("게시글 보기 성공")
+    @Test
+    void view_notice_success() throws Exception {
+        //given
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setUsername("ljseokd");
+        signUpForm.setPassword("12341234");
+        Long createdId = accountService.join(signUpForm);
+        Account account = accountRepository.findById(createdId).get();
+
+        NoticeForm noticeForm = new NoticeForm();
+        noticeForm.setTitle("title");
+        noticeForm.setContents("contents");
+        Long noticeId = noticeService.createNotice(account, noticeForm);
+        Notice notice = noticeRepository.findById(noticeId).get();
+
+        //when
+        mockMvc.perform(get("/notice/" + noticeId + "/view"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("isWriter"))
+                .andExpect(model().attributeExists("notice"))
+                .andExpect(view().name("notice/view"));
+
+        //then
+        assertTrue(notice.isWriter(account));
+    }
+
+    @DisplayName("게시글 보기 실패 (없는 경로)")
+    @Test
+    void view_notice_fail() throws Exception {
+        assertThrows(Exception.class,
+                () -> mockMvc.perform(get("/notice/123/view"))) ;
     }
 
 }

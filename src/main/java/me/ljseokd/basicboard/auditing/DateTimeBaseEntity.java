0f3f6a698dceec5bcf45cdb2c4cada4666acd5a9
JoongSeokD <ljseokd@gmail.com>
2020-07-30 17:35:29 +0900

    생성시간 수정 시간 자동 생성 Base클래스

---
+++ src/main/java/me/ljseokd/basicboard/auditing/DateTimeBaseEntity.java
@@ -0,0 +1,33 @@
+package me.ljseokd.basicboard.auditing;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class DateTimeBaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDateTime;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedTime;
+
+    @PrePersist
+    private void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createDateTime = now;
+        lastModifiedTime = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        lastModifiedTime = LocalDateTime.now();
+    }
+}

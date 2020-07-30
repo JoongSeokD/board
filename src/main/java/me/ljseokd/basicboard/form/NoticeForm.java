121a3503e7f44444d297ae416b384508495e5026
JoongSeokD <ljseokd@gmail.com>
2020-07-30 11:25:26 +0900

    게시글 등록 폼 데이터 안쓰는 import 제거

--- src/main/java/me/ljseokd/basicboard/form/NoticeForm.java
+++ src/main/java/me/ljseokd/basicboard/form/NoticeForm.java
@@ -1,19 +1,10 @@
 package me.ljseokd.basicboard.form;
 
 import lombok.Data;
-import me.ljseokd.basicboard.domain.Account;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-
-import static javax.persistence.FetchType.LAZY;
 
 @Data
 public class NoticeForm {

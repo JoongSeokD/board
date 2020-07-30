b8d8b9ec8d26772e96054506f26fcc2e4b6c2f67
JoongSeokD <ljseokd@gmail.com>
2020-07-30 15:31:29 +0900

    static 파일 시큐리티 ignore

--- src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
+++ src/main/java/me/ljseokd/basicboard/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package me.ljseokd.basicboard.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -20,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().mvcMatchers("/", "/login", "sign-up").permitAll()
+                .authorizeRequests().mvcMatchers("/", "/login", "/sign-up").permitAll()
                 .anyRequest().authenticated();
 
         http
@@ -29,5 +31,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }

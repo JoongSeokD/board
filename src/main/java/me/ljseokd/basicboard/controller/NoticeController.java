38888b828ea869e1c7f1a894e169fa647df90130
JoongSeokD <ljseokd@gmail.com>
2020-07-30 11:25:11 +0900

    게시글 등록

--- src/main/java/me/ljseokd/basicboard/controller/NoticeController.java
+++ src/main/java/me/ljseokd/basicboard/controller/NoticeController.java
@@ -1,16 +1,46 @@
 package me.ljseokd.basicboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.ljseokd.basicboard.domain.Account;
+import me.ljseokd.basicboard.domain.CurrentAccount;
 import me.ljseokd.basicboard.form.NoticeForm;
+import me.ljseokd.basicboard.service.NoticeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/notice")
 public class NoticeController {
 
-    @GetMapping("/notice/new")
+
+    private final NoticeService noticeService;
+
+    @GetMapping("/new")
     public String createNoticeForm(Model model){
         model.addAttribute(new NoticeForm());
         return "notice/create-notice-form";
+    }
+
+    @PostMapping("/new")
+    public String createNotice(@CurrentAccount Account account,
+                               @Valid @ModelAttribute NoticeForm noticeForm,
+                               Errors errors,
+                               Model model){
+
+        if (errors.hasErrors()){
+            model.addAttribute("noticeForm",noticeForm);
+            return "/notice/create-notice-form";
+        }
+
+        Long createdId = noticeService.createNotice(account, noticeForm);
+        return "redirect:/notice/" + createdId + "/view";
     }
 }

b2577294b29655c59340cd96daab3c9a9626fb72
JoongSeokD <ljseokd@gmail.com>
2020-08-03 10:17:05 +0900

    게시글 목록 dto

---
+++ src/main/java/me/ljseokd/basicboard/dto/NoticeDto.java
@@ -0,0 +1,23 @@
+package me.ljseokd.basicboard.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class NoticeDto {
+
+    private String writer;
+    private String title;
+    private LocalDateTime lastModifiedTime;
+    private Long noticeId;
+
+    @QueryProjection
+    public NoticeDto(String writer, String title, LocalDateTime lastModifiedTime, Long noticeId) {
+        this.writer = writer;
+        this.title = title;
+        this.lastModifiedTime = lastModifiedTime;
+        this.noticeId = noticeId;
+    }
+}

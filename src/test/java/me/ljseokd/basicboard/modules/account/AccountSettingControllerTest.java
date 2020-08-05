a22a0ed63c38d544b566a9e8ccf4b98fb093e96d
JoongSeokD <ljseokd@gmail.com>
2020-08-05 17:40:27 +0900

    description 페이지 테스트 케이스

--- src/test/java/me/ljseokd/basicboard/modules/account/AccountSettingControllerTest.java
+++ src/test/java/me/ljseokd/basicboard/modules/account/AccountSettingControllerTest.java
@@ -1,13 +1,20 @@
 package me.ljseokd.basicboard.modules.account;
 
 import me.ljseokd.basicboard.infra.MockMvcTest;
+import me.ljseokd.basicboard.modules.main.form.SignUpForm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @MockMvcTest
@@ -16,15 +23,64 @@ class AccountSettingControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @DisplayName("나의 계정 설정 메인 페이지")
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void beForeEach(){
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setUsername("ljseokd");
+        signUpForm.setPassword("12341234");
+        accountService.join(signUpForm);
+    }
+
+    @DisplayName("내 소개 설정 페이지")
     @Test
-    @WithAccount("ljseokd")
-    void settings_main_page() throws Exception {
-        mockMvc.perform(get("/settings"))
+    void descriptionForm() throws Exception {
+        mockMvc.perform(get("/settings/description"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/settings"))
-                .andExpect(model().attributeExists("profileDto"))
-                .andExpect(authenticated());
+                .andExpect(model().attributeExists("profileForm"))
+                .andExpect(view().name("account/settings/description"));
+    }
+
+    @Test
+    @DisplayName("내 소개 설정 성공")
+    void descriptionSubmit_success() throws Exception {
+        Account account = accountRepository.findByNickname("ljseokd").get();
+        String findBio = account.getBio();
+        String setBio = "안녕하세요 잘 부탁드립니다.";
+
+        mockMvc.perform(post("/settings/description")
+                .param("bio", setBio)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(model().hasNoErrors())
+                .andExpect(view().name("redirect:/settings/description"));
+
+        assertNull(findBio);
+        assertNotEquals(findBio, setBio);
+    }
+
+    @Test
+    @DisplayName("내 소개 설정 실패 (bio 길이 초과)")
+    void descriptionSubmit_fail() throws Exception {
+        Account account = accountRepository.findByNickname("ljseokd").get();
+        String findBio = account.getBio();
+        String setBio = "안녕하세요 잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.잘 부탁드립니다.";
+
+        mockMvc.perform(post("/settings/description")
+                .param("bio", setBio)
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("profileForm"))
+                .andExpect(view().name("account/settings/description"));
+
+        assertNull(findBio);
+        assertEquals(findBio, account.getBio());
     }
 
 }

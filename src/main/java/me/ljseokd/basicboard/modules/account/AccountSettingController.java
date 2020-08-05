37d9cce2aad5225c44c91dd22fd10cc4f2c16e4c
JoongSeokD <ljseokd@gmail.com>
2020-08-05 17:40:10 +0900

    description 수정 페이지 추가

--- src/main/java/me/ljseokd/basicboard/modules/account/AccountSettingController.java
+++ src/main/java/me/ljseokd/basicboard/modules/account/AccountSettingController.java
@@ -1,13 +1,20 @@
 package me.ljseokd.basicboard.modules.account;
 
 import lombok.RequiredArgsConstructor;
-import me.ljseokd.basicboard.modules.account.dto.ProfileDto;
+import me.ljseokd.basicboard.modules.account.form.ProfileForm;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/settings")
 @RequiredArgsConstructor
 public class AccountSettingController {
 
@@ -15,15 +22,41 @@ public class AccountSettingController {
 
     private final ModelMapper modelMapper;
 
-    @GetMapping("/settings")
-    public String settingsMainPage(@CurrentAccount Account account, Model model){
-        Account findByNickname = accountRepository.findByNickname(account.getNickname())
-                .orElseThrow(() -> new IllegalArgumentException(String.valueOf(account.getId())));
+    private static final String ACCOUNT = "account";
+    private static final String SETTINGS = "/settings";
+    private static final String DESCRIPTION = "/description";
 
-        model.addAttribute("profileDto", modelMapper.map(findByNickname, ProfileDto.class));
+    @GetMapping(DESCRIPTION)
+    public String descriptionForm(@CurrentAccount Account account, Model model){
+        Account findByNickname = getFindByNickname(account.getNickname());
 
-        return "account/settings";
+        model.addAttribute("profileForm", modelMapper.map(findByNickname, ProfileForm.class));
 
+        return ACCOUNT + SETTINGS + DESCRIPTION;
     }
+
+    private Account getFindByNickname(String nickname) {
+        return accountRepository.findByNickname(nickname)
+                .orElseThrow(() -> new IllegalArgumentException(nickname));
+    }
+
+    @PostMapping(DESCRIPTION)
+    public String descriptionSubmit(@CurrentAccount Account account,
+                                    @Valid @ModelAttribute ProfileForm profileForm,
+                                    Errors errors, Model model){
+
+        if (errors.hasErrors()){
+            model.addAttribute(profileForm);
+            return ACCOUNT + SETTINGS + DESCRIPTION;
+        }
+
+        Account findAccount = getFindByNickname(account.getNickname());
+        findAccount.changeDescription(profileForm);
+
+        return "redirect:" + SETTINGS + DESCRIPTION;
+    }
+
+
+
 
 }

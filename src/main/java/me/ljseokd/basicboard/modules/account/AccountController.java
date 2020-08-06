f821eeb4c33e67eb3587d95446bd380eebca6468
JoongSeokD <ljseokd@gmail.com>
2020-08-06 12:17:18 +0900

    회원 가입 코드 이동 MainController -> AccountController

--- src/main/java/me/ljseokd/basicboard/modules/account/AccountController.java
+++ src/main/java/me/ljseokd/basicboard/modules/account/AccountController.java
@@ -2,18 +2,32 @@ package me.ljseokd.basicboard.modules.account;
 
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.basicboard.modules.account.dto.ProfileDto;
+import me.ljseokd.basicboard.modules.main.form.SignUpForm;
+import me.ljseokd.basicboard.modules.main.validator.SignUpFormValidator;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class AccountController {
 
-    private final AccountRepository accountRepository;
     private final ModelMapper modelMapper;
+    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+
 
     @GetMapping(value = "/profile/{nickname}")
     public String viewProfile(@PathVariable String nickname,
@@ -26,6 +40,22 @@ public class AccountController {
         model.addAttribute("profileDto", modelMapper.map(viewAccount, ProfileDto.class));
 
         return "account/profile";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model){
+        model.addAttribute(new SignUpForm());
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){
+
+        if (errors.hasErrors()){
+            return "sign-up";
+        }
+        accountService.join(signUpForm);
+        return "redirect:/";
     }
 
 }
--- src/test/java/me/ljseokd/basicboard/modules/account/AccountControllerTest.java
+++ src/test/java/me/ljseokd/basicboard/modules/account/AccountControllerTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +24,65 @@ class AccountControllerTest {
 
     @Autowired
     AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+
+    @DisplayName("회원 가입 페이지")
+    @Test
+    void sign_up_form() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sign-up"))
+                .andDo(print())
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 성공")
+    @Test
+    void sign_up_success() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andDo(print())
+                .andExpect(model().hasNoErrors())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated())
+        ;
+
+        Account ljseokd = accountRepository.findByNickname("ljseokd").get();
+        Assertions.assertNotEquals("12345678",ljseokd.getPassword());
+    }
+
+    @DisplayName("회원가입 실패 (중복된 이름)")
+    @Test
+    void sign_up_duplicate_fail() throws Exception {
+        accountRepository.save(new Account("ljseokd", "12345678"));
+
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(view().name("sign-up"))
+                .andExpect(unauthenticated());
+    }
+    @DisplayName("회원가입 실패 (비밀번호 길이 부족)")
+    @Test
+    void sign_up_password_length_fail() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("username", "ljseokd")
+                .param("password", "1234567")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(view().name("sign-up"))
+                .andExpect(unauthenticated());
+    }
 
 
     @Test

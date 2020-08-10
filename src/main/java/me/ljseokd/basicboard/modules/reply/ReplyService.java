4a811c104520e59dfbb3a8b0489a4e1591c525a1
JoongSeokD <ljseokd@gmail.com>
2020-08-10 11:18:29 +0900

    댓글 추가 로직

---
+++ src/main/java/me/ljseokd/basicboard/modules/reply/ReplyService.java
@@ -0,0 +1,30 @@
+package me.ljseokd.basicboard.modules.reply;
+
+import lombok.RequiredArgsConstructor;
+import me.ljseokd.basicboard.modules.account.Account;
+import me.ljseokd.basicboard.modules.account.AccountRepository;
+import me.ljseokd.basicboard.modules.notice.Notice;
+import me.ljseokd.basicboard.modules.notice.NoticeRepository;
+import me.ljseokd.basicboard.modules.reply.form.ReplyForm;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReplyService {
+
+    private final NoticeRepository noticeRepository;
+    private final AccountRepository accountRepository;
+
+
+    public void addReply(Account account, Long noticeId, ReplyForm replyForm) {
+        String nickname = account.getNickname();
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new IllegalArgumentException(String.valueOf(noticeId)));
+        Account findAccount = accountRepository.findByNickname(nickname)
+                .orElseThrow(() -> new IllegalArgumentException(nickname));
+
+        new Reply(findAccount, notice, replyForm.getContents());
+    }
+}

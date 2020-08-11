b29b5be441bade9f04ab0d0b07672da54fae5c90
JoongSeokD <ljseokd@gmail.com>
2020-08-11 10:20:30 +0900

    댓글 리스트 dto

---
+++ src/main/java/me/ljseokd/basicboard/modules/reply/dto/ReplyDto.java
@@ -0,0 +1,24 @@
+package me.ljseokd.basicboard.modules.reply.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ReplyDto {
+
+    private Long id;
+    private String writer;
+    private LocalDateTime createdDate;
+    private String contents;
+    private boolean isOwner;
+
+    @QueryProjection
+    public ReplyDto(Long id, String writer, LocalDateTime createdDate, String contents) {
+        this.id = id;
+        this.writer = writer;
+        this.createdDate = createdDate;
+        this.contents = contents;
+    }
+}

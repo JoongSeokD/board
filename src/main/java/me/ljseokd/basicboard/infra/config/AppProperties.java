f399c3464bb2712ca3f20325981e0f2ec566220c
JoongSeokD <ljseokd@gmail.com>
2020-08-12 17:20:44 +0900

    사용자 정의 프로퍼티스 클래스

---
+++ src/main/java/me/ljseokd/basicboard/infra/config/AppProperties.java
@@ -0,0 +1,12 @@
+package me.ljseokd.basicboard.infra.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private String path;
+}

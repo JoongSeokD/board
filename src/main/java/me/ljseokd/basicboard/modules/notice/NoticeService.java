17156b04ecccf2cc41f381e1b3ed711668257620
JoongSeokD <ljseokd@gmail.com>
2020-08-14 15:52:41 +0900

    게시글 삭제 cascade 최적화

--- src/main/java/me/ljseokd/basicboard/modules/notice/NoticeService.java
+++ src/main/java/me/ljseokd/basicboard/modules/notice/NoticeService.java
@@ -3,7 +3,9 @@ package me.ljseokd.basicboard.modules.notice;
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.basicboard.modules.account.Account;
 import me.ljseokd.basicboard.modules.account.AccountRepository;
+import me.ljseokd.basicboard.modules.file.AttacheFileRepository;
 import me.ljseokd.basicboard.modules.notice.form.NoticeForm;
+import me.ljseokd.basicboard.modules.reply.ReplyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,8 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private final AccountRepository accountRepository;
+    private final ReplyRepository replyRepository;
+    private final AttacheFileRepository attacheFileRepository;
 
 
     public Long createNotice(Account account, NoticeForm noticeForm) {
@@ -36,4 +40,23 @@ public class NoticeService {
 
         notice.update(noticeForm);
     }
+
+    public String delete(Long noticeId) {
+        Notice notice = noticeRepository.findByIdFetchFileAndReply(noticeId)
+                .orElseThrow(()-> new IllegalArgumentException(String.valueOf(noticeId)));
+
+        if (notice.getReplyList().size() > 0){
+            replyRepository.deleteInBatch(notice.getReplyList());
+            notice.getReplyList().clear();
+        }
+        if (notice.getFileList().size() > 0){
+            attacheFileRepository.deleteInBatch(notice.getFileList());
+            notice.getFileList().clear();
+        }
+
+        noticeRepository.delete(notice);
+        return notice.getTitle();
+    }
+
+
 }

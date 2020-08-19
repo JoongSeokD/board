0f8fea1d9ef8518204753a4b9e2bb265d4bec77b
JoongSeokD <ljseokd@gmail.com>
2020-08-19 14:42:49 +0900

    댓글 작성 알람 이벤트 추가

--- src/main/java/me/ljseokd/basicboard/modules/notice/reply/ReplyService.java
+++ src/main/java/me/ljseokd/basicboard/modules/notice/reply/ReplyService.java
@@ -3,9 +3,9 @@ package me.ljseokd.basicboard.modules.notice.reply;
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.basicboard.modules.account.Account;
 import me.ljseokd.basicboard.modules.account.AccountRepository;
-import me.ljseokd.basicboard.modules.notice.event.ReplyCreatedEvent;
 import me.ljseokd.basicboard.modules.notice.Notice;
 import me.ljseokd.basicboard.modules.notice.NoticeRepository;
+import me.ljseokd.basicboard.modules.notice.event.ReplyCreatedEvent;
 import me.ljseokd.basicboard.modules.notice.reply.form.ReplyForm;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -23,12 +23,12 @@ public class ReplyService {
 
     public void addReply(Account account, Long noticeId, ReplyForm replyForm) {
         String nickname = account.getNickname();
-        Notice notice = noticeRepository.findByIdFetchReply(noticeId)
+        Notice notice = noticeRepository.findById(noticeId)
                 .orElseThrow(() -> new IllegalArgumentException(String.valueOf(noticeId)));
         Account findAccount = accountRepository.findByNickname(nickname)
                 .orElseThrow(() -> new IllegalArgumentException(nickname));
         Reply newReply = new Reply(findAccount, notice, replyForm.getContents());
-        // TODO 댓글을 작성하면 댓글 작성자 이외의 사람에게 알림이 가야함
+
         eventPublisher.publishEvent(new ReplyCreatedEvent(newReply));
     }
 

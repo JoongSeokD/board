711777f45dc29c24cca8c40a4acab7c846719c1b
JoongSeokD <ljseokd@gmail.com>
2020-08-19 14:45:42 +0900

    알림 받을 Account 조회용 쿼리dsl 인터페이스

--- src/main/java/me/ljseokd/basicboard/modules/notice/NoticeRepositoryCustom.java
+++ src/main/java/me/ljseokd/basicboard/modules/notice/NoticeRepositoryCustom.java
@@ -1,11 +1,15 @@
 package me.ljseokd.basicboard.modules.notice;
 
+import me.ljseokd.basicboard.modules.account.Account;
 import me.ljseokd.basicboard.modules.notice.dto.NoticeDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface NoticeRepositoryCustom {
 
     Page<NoticeDto> page(Pageable pageable);
 
+    List<Account> findAccountWithNotificationById(Long id, Long writerId);
 }

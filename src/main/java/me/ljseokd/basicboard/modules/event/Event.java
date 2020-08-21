125d9beecd6c61fcf73f9b3ff9a1551d7b02aacb
JoongSeokD <ljseokd@gmail.com>
2020-08-21 10:54:01 +0900

    행사 도메인

---
+++ src/main/java/me/ljseokd/basicboard/modules/event/Event.java
@@ -0,0 +1,65 @@
+package me.ljseokd.basicboard.modules.event;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.ljseokd.basicboard.modules.account.Account;
+import me.ljseokd.basicboard.modules.event.form.EventForm;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Event {
+
+    @Id @GeneratedValue
+    @Column(name = "event_id")
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = LAZY)
+    private Account createdBy;
+
+    @Lob
+    private String contents;
+
+    private LocalDateTime recruitmentStartDate;
+    private LocalDateTime recruitmentEndDate;
+    private LocalDateTime eventsStartDate;
+    private LocalDateTime eventsEndDate;
+
+    private long limitOfEnrollments;
+
+    @Lob
+    private String thumbnail;
+
+    private boolean free;
+
+    private int price;
+
+    @Enumerated(STRING)
+    private EventType eventType;
+
+    public Event(Account account, EventForm eventForm) {
+        this.createdBy = account;
+        this.title = eventForm.getTitle();
+        this.contents = eventForm.getContents();
+        this.limitOfEnrollments = eventForm.getLimitOfEnrollments();
+        this.free = eventForm.isFree();
+        if (!eventForm.isFree()){
+            price = eventForm.getPrice();
+        }
+        this.eventType = eventForm.getEventType();
+        this.thumbnail = eventForm.getThumbnail();
+        this.recruitmentStartDate = eventForm.getRecruitmentStartDate();
+        this.recruitmentEndDate = eventForm.getRecruitmentEndDate();
+        this.eventsStartDate = eventForm.getEventsStartDate();
+        this.eventsEndDate = eventForm.getEventsEndDate();
+    }
+}

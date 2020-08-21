65e0ed4f51e24e4a72b373b98e23b1aeaf8d62ab
JoongSeokD <ljseokd@gmail.com>
2020-08-21 16:00:18 +0900

    행사 이벤트 상세 페이지 Dto용 select 쿼리 추가

--- src/main/java/me/ljseokd/basicboard/modules/event/EventRepository.java
+++ src/main/java/me/ljseokd/basicboard/modules/event/EventRepository.java
@@ -1,8 +1,14 @@
 package me.ljseokd.basicboard.modules.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
+
+    @Query("select e from Event e  join fetch e.createdBy where e.id = ?1")
+    Optional<Event> findByAccountWithId(Long eventId);
 }

e54229144c16a9d207d346e14be87f1fb52b931c
JoongSeokD <ljseokd@gmail.com>
2020-08-21 15:58:47 +0900

    행사 상세 페이지 추가

--- src/main/java/me/ljseokd/basicboard/modules/event/EventController.java
+++ src/main/java/me/ljseokd/basicboard/modules/event/EventController.java
@@ -3,20 +3,20 @@ package me.ljseokd.basicboard.modules.event;
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.basicboard.modules.account.Account;
 import me.ljseokd.basicboard.modules.account.CurrentAccount;
+import me.ljseokd.basicboard.modules.event.dto.EventViewDto;
 import me.ljseokd.basicboard.modules.event.form.EventForm;
 import me.ljseokd.basicboard.modules.event.validator.EventFormValidator;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,8 +24,8 @@ public class EventController {
 
     private final EventService eventService;
     private final EventRepository eventRepository;
-
     private final EventFormValidator eventFormValidator;
+    private final ModelMapper modelMapper;
 
     @InitBinder("eventForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -54,7 +54,31 @@ public class EventController {
             model.addAttribute(eventForm);
             return "event/new";
         }
-        eventService.newEvent(account, eventForm);
-        return "redirect:/events";
+        Long newEventId = eventService.newEvent(account, eventForm);
+        return "redirect:/events/" + newEventId + "/view";
     }
+
+    @GetMapping("/events/{eventId}/view")
+    public String viewEvent(@PathVariable Long eventId, Model model,
+                            @CurrentAccount Account account){
+
+        Event event = eventRepository.findByAccountWithId(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("/events/" + eventId + "/view 는 없는 행사 번호입니다."));
+
+        model.addAttribute("eventViewDto", modelMapper.map(event, EventViewDto.class));
+        model.addAttribute("isEnrollmentValidate", isEnrollmentValidate(account, event));
+        model.addAttribute("isOwner", isOwner(account, event));
+
+        return "event/view";
+    }
+
+    // 행사 작성자가 아니고, 접수 마감 전인지 검증
+    private boolean isEnrollmentValidate(Account account, Event event) {
+        return !isOwner(account, event) && !LocalDateTime.now().isBefore(event.getRecruitmentEndDate());
+    }
+
+    private boolean isOwner(Account account, Event event) {
+        return event.getCreatedBy().isOwner(account);
+    }
+
 }

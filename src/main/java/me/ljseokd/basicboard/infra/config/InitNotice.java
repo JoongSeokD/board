b2148a5e7c3638bf57b161286bfdd74938f5bd44
JoongSeokD <ljseokd@gmail.com>
2020-08-21 10:56:12 +0900

    개발 환경 행사 테스트 데이터

--- src/main/java/me/ljseokd/basicboard/infra/config/InitNotice.java
+++ src/main/java/me/ljseokd/basicboard/infra/config/InitNotice.java
@@ -2,6 +2,9 @@ package me.ljseokd.basicboard.infra.config;
 
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.basicboard.modules.account.Account;
+import me.ljseokd.basicboard.modules.event.Event;
+import me.ljseokd.basicboard.modules.event.EventType;
+import me.ljseokd.basicboard.modules.event.form.EventForm;
 import me.ljseokd.basicboard.modules.notice.Notice;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 
 @Profile("dev")
 @Component
@@ -32,9 +36,18 @@ public class InitNotice {
         public void init(){
             Account test = new Account("test", "12341234");
             em.persist(test);
-
             for (int i = 0; i < 100; i++){
                 em.persist(new Notice("title" + i, "contents" + i, test));
+                EventForm eventForm = new EventForm();
+                eventForm.setTitle("title" + i);
+                eventForm.setContents("contents" + i);
+                eventForm.setFree(true);
+                eventForm.setRecruitmentStartDate(LocalDateTime.now().plusDays(1));
+                eventForm.setRecruitmentEndDate(LocalDateTime.now().plusDays(2));
+                eventForm.setEventsStartDate(LocalDateTime.now().plusDays(3));
+                eventForm.setEventsEndDate(LocalDateTime.now().plusDays(4));
+                em.persist(new Event(test, eventForm));
+
             }
         }
     }
